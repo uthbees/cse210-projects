3aaf71f706a0906d58408e99917723114f9950ca
Ethan Ball <uthbees@gmail.com>
2024-01-10 11:03:38 -0700

    Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        var firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        var lastName = Console.ReadLine();
+
+        Console.WriteLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }

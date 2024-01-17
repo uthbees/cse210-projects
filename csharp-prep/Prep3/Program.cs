fc3fcc9632076a385f6ea88196b643021e957410
Ethan Ball <uthbees@gmail.com>
2024-01-17 11:12:39 -0700

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        var randomGenerator = new Random();
+        int target = randomGenerator.Next(1, 101);
+
+        while (true)
+        {
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess < target)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (guess > target)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                break;
+            }
+        }
     }
 }

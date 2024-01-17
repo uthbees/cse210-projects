bfcce6e02a631a38c441e1ca913a65ec138ec793
Ethan Ball <uthbees@gmail.com>
2024-01-17 11:59:31 -0700

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        var list = new List<int>();
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            int newInput = int.Parse(Console.ReadLine());
+
+            if (newInput != 0)
+            {
+                list.Add(newInput);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        var sum = list.Sum();
+        double average = (double)sum / list.Count;
+        var max = list.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }

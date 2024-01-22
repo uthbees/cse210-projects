483a92a1051fc806aa50f8464abbc93384321cf3
Ethan Ball <uthbees@gmail.com>
2024-01-22 11:43:49 -0700

    Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        var job1 = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = "2020";
+        job1._endYear = "2021";
+        
+        var job2 = new Job();
+        job2._company = "Microsoft";
+        job2._jobTitle = "Sysadmin";
+        job2._startYear = "2019";
+        job2._endYear = "2022";
+        
+        var resume = new Resume();
+        resume._name = "John Doe";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+        
+        resume.Display();
     }
 }
